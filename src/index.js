47c870b0f9e3cc5818a9394e0d37652930a6666c
Hector <hector.quijada2121@gmail.com>
2022-02-05 01:19:12 -0500

    fix button component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import './input.css';
+import './styles/tailwind.css';
 import App from './App';
 
 ReactDOM.render(
