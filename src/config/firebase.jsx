c6fa4056501d0125c0322893ad0c82932f393e23
Hector <hector.quijada2121@gmail.com>
2022-07-04 20:11:11 -0400

    opt-out of  jsxRuntime due to bugs

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: process.env.VITE__API_KEY,
-  authDomain: process.env.VITE__AUTH_DOMAIN,
-  databaseURL: process.env.VITE__DATABASE_URL,
-  projectId: process.env.VITE__PROJECT_ID,
-  storageBucket: process.env.VITE__STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE__MESSAGING_SENDER_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID
 };
 
 // Initialize Firebase
